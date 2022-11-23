2f9d3b5ecc66c3dfa89348f23c74efee48dbc237
ithuizai <wmh18306679607@163.com>
2022-11-23 09:19:03 +0800

    完成个人中心的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -14,7 +14,7 @@ $.ajaxPrefilter(function(options){
     // 全局统一挂载complete回调函数
     options.complete = function(res){
         //在complete回调函数中，可以使用res.responseJSON拿到服务器响应回来的数据
-        if(res.responseJSON.status === 1 && res.responseJSON.   message === '身份认证失败！'){
+        if(res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！'){
             //1、强制清空token
             localStorage.removeItem('token')
             //2、强制跳转到登录页面
